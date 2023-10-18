533dcba1f7bb77c875ec5f0db8fd802575b6e9ee
Leonardo Vieira <leonardojcv@Outlook.com>
2023-10-18 17:45:42 -0300

    First commit

---
+++ src/test/java/com/leonardojcv/springmongo/SpringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardojcv.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
