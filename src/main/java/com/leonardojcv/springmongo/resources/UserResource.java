f7905a876f404308378447a01eb880a71037d3f3
Leonardo Vieira <leonardojcv@Outlook.com>
2023-10-26 18:27:11 -0300

    User delete

--- src/main/java/com/leonardojcv/springmongo/resources/UserResource.java
+++ src/main/java/com/leonardojcv/springmongo/resources/UserResource.java
@@ -6,7 +6,13 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.leonardojcv.springmongo.domain.User;
@@ -31,6 +37,12 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void>delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 	@PostMapping
--- src/main/java/com/leonardojcv/springmongo/services/UserService.java
+++ src/main/java/com/leonardojcv/springmongo/services/UserService.java
@@ -13,24 +13,30 @@ import com.leonardojcv.springmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	public UserRepository repository;
-	
-	public List<User>findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
+
 }
