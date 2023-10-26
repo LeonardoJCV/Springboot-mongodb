de95be9ef64775fb17adc00e20b5ac965caf2359
Leonardo Vieira <leonardojcv@Outlook.com>
2023-10-26 18:17:37 -0300

    Adding user with POST

--- src/main/java/com/leonardojcv/springmongo/services/UserService.java
+++ src/main/java/com/leonardojcv/springmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leonardojcv.springmongo.domain.User;
+import com.leonardojcv.springmongo.dto.UserDTO;
 import com.leonardojcv.springmongo.repository.UserRepository;
 import com.leonardojcv.springmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
